3718b3cb4b1caddf2c8a4fe7f3e4f6ba2cf1f04f
ShaunQ0 <quadraoshaun@gmail.com>
2025-03-03 15:43:11 +0100

    Background ajouter
    version 1.0

--- SAE406/Assets/Script/playermovement.cs
+++ SAE406/Assets/Script/playermovement.cs
@@ -17,6 +17,7 @@ public class playermovement : MonoBehaviour
     public int currentNumbersJumps = 0;
     public bool isFacingRight = false;
     public VoidEventChannel onPlayerDeath;
+
     private void OnEnable() {
         onPlayerDeath.OnEventRaised += Die;
     }
@@ -24,73 +25,72 @@ public class playermovement : MonoBehaviour
     private void OnDisable() {
         onPlayerDeath.OnEventRaised -= Die;   
     }
+
     void Start()
     {
         
     }
+
     void Die() {
         bc.enabled = false;
         rb.bodyType = RigidbodyType2D.Static;
         enabled = false;
     }
+
     // Update is called once per frame
     void Update()
     {
+        if (Time.timeScale == 0) return; // Bloquer les actions si le jeu est en pause
+
         moveDirectionX = Input.GetAxis("Horizontal");
- // Vérifiez si le joueur appuie sur le bouton de saut
-        if(Input.GetButtonDown("Jump") && 
-        currentNumbersJumps < maxAllowedJumps ) 
+
+        // Vérifiez si le joueur appuie sur le bouton de saut
+        if (Input.GetButtonDown("Jump") && currentNumbersJumps < maxAllowedJumps) 
         {
             Jump();
             currentNumbersJumps++;
         }
 
-        if (
-            isGrounded && 
-            !Input.GetButton("Jump")
-        ) {
+        if (isGrounded && !Input.GetButton("Jump")) 
+        {
             currentNumbersJumps = 0;
         }
+
         Flip();
     }
-     void Flip() {
-        if (
-        (moveDirectionX > 0 && !isFacingRight) ||
-        (moveDirectionX < 0 && isFacingRight)
-        ) {
+
+    void Flip() {
+        if ((moveDirectionX > 0 && !isFacingRight) || (moveDirectionX < 0 && isFacingRight)) 
+        {
             transform.Rotate(0, 180, 0);
             isFacingRight = !isFacingRight;
         }
     }
-      private void Jump() 
+
+    private void Jump() 
     {
-        rb.linearVelocity = new Vector2(
-            rb.linearVelocity.x,
-            jumpForce
-        );
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
     }
+
     private void FixedUpdate () {
-        rb.linearVelocity = new Vector2 (
-            moveDirectionX * moveSpeed,
-            rb.linearVelocity.y 
-        );
+        if (Time.timeScale == 0) 
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y); // Arrêter les mouvements horizontaux pendant la pause
+            return;
+        }
+
+        rb.velocity = new Vector2(moveDirectionX * moveSpeed, rb.velocity.y);
         isGrounded = IsGrounded();
     }
 
     public bool IsGrounded() {
-        return Physics2D.OverlapCircle(
-            groundCheck.position,
-            groundCheckRadius,
-            ListGroundlayers
-        );
+        return Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, ListGroundlayers);
     }
+
     private void OnDrawGizmos () {
         if (groundCheck != null) {
             Gizmos.color = Color.magenta;
-            Gizmos.DrawWireSphere (
-                groundCheck.position,
-                groundCheckRadius
-            );
+            Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
         }
     }
 }
