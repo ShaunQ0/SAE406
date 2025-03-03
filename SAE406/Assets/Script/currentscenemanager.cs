ea687a435df6d47367225505fbe565619eacbe47
ShaunQ0 <quadraoshaun@gmail.com>
2025-03-03 15:29:33 +0100

    Button Reprendre

--- SAE406/Assets/Script/currentscenemanager.cs
+++ SAE406/Assets/Script/currentscenemanager.cs
@@ -1,52 +1,69 @@
-using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class currentscenemanager : MonoBehaviour
+
+public class CurrentSceneManager : MonoBehaviour
 {
     public GameObject gameOverScreen;
+    public GameObject pauseMenu;
     public VoidEventChannel onPlayerDeath;
+    
+    private bool isPaused = false;
+
     private void OnEnable()
     {
         onPlayerDeath.OnEventRaised += Die;
     }
+
     private void OnDisable()
     {
         onPlayerDeath.OnEventRaised -= Die;
     }
+
     private void Die()
     {
         gameOverScreen.SetActive(true);
+        Time.timeScale = 0; // Stop le temps quand le joueur meurt
     }
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
         gameOverScreen.SetActive(false);
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1; // Assurez-vous que le jeu commence normalement
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            if (Time.timeScale == 0)
-            {
-                Time.timeScale = 1;
-            }
-            else
-            {
-                Time.timeScale = 0;
-            }
+            TogglePause();
         }
+
         if (Input.GetKeyDown(KeyCode.R))
         {
             RestartGame();
         }
-
     }
+
+    public void TogglePause()
+    {
+        isPaused = !isPaused;
+        
+        if (isPaused)
+        {
+            Time.timeScale = 0;
+            pauseMenu.SetActive(true);
+        }
+        else
+        {
+            Time.timeScale = 1;
+            pauseMenu.SetActive(false);
+        }
+    }
+
     public void RestartGame()
     {
-        SceneManager.LoadScene(
-            SceneManager.GetActiveScene().name
-        );
+        Time.timeScale = 1; // Remettre le jeu en vitesse normale
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
